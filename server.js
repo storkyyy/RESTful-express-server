a8d5d0210e14ace4ae5807541d094b039a5ef9a3
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2021-11-06 09:59:05 -0400

    Correct formatting

--- server.js
+++ server.js
@@ -22,7 +22,13 @@ app.use((req, res) => {
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>')
+    res.end(`
+        <html>
+            <body>
+                <h1>This is an Express Server</h1>
+            </body>
+        </html>
+    `);
 
 });
 
